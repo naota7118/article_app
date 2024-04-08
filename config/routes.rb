1c5bc4c02158b1f50ae873cdf805ba68bb4bca3d
naota7118 <naota7118@gmail.com>
2024-04-08 09:51:11 +0000

    ホーム画面からユーザー登録画面に遷移できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
-  get "static_pages/home"
-  get 'static_pages/create'
-  get 'static_pages/article'
+  root "articles#index"
   resources :users
+  resources :articles
 end
