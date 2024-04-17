9202311376bc23ea300b3e910cde69a5070f987e
naota7118 <naota7118@gmail.com>
2024-04-17 09:27:52 +0000

    Articleモデルのバリデーションテストを追加

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@foobar.com",
+      password: "foobar", password_confirmation: "foobar")
+    @user.save
+    @article = Article.new(title: "今日の天気", junre: "ブログ", content: "今日は晴れています。", tag: "天気", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @article.valid?
+  end
 end
